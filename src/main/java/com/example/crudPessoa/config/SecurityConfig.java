9f6bca6f02b2ef35806a74cc10d9b8f5b4005634
leonardo.lopes <leonardo.lopes@spcbrasil.org.br>
2023-06-12 14:35:11 -0300

    adciona perfils au usuarios

--- src/main/java/com/example/crudPessoa/config/SecurityConfig.java
+++ src/main/java/com/example/crudPessoa/config/SecurityConfig.java
@@ -1,2 +1,59 @@
-package com.example.crudPessoa.config;public class SecurityConfig {
+package com.example.crudPessoa.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+public class SecurityConfig {
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/h2-console/**"
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/produtos/**",
+            "/categorias/**",
+            "/pessoas/**"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return
+                http.cors().and().csrf().disable().authorizeRequests()
+                        .requestMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                        .requestMatchers(antMatcher(PUBLIC_MATCHERS[0])).permitAll().
+                        anyRequest().authenticated().and().headers().frameOptions().sameOrigin()
+                        .and().build();
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
 }
--- src/main/java/com/example/crudPessoa/entities/Pessoa.java
+++ src/main/java/com/example/crudPessoa/entities/Pessoa.java
@@ -1,19 +1,21 @@
 package com.example.crudPessoa.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.crudPessoa.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Entity
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 public class Pessoa implements Serializable {
     @Id
@@ -21,9 +23,34 @@ public class Pessoa implements Serializable {
     private UUID id;
     private String nome;
     private String email;
+    @JsonIgnore
     private String telefone;
     private int numeroDocumneto;
+    private String senha;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "PERFILS")
+    private Set<Integer> perfils = new HashSet<>();
 
     public Pessoa() {
+        addPerfil(Perfil.COMU);
+    }
+
+    public Pessoa(UUID id, String nome, String email, String telefone, int numeroDocumneto, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.numeroDocumneto = numeroDocumneto;
+        this.senha = senha;
+        this.perfils = perfils;
+        addPerfil(Perfil.COMU);
+    }
+
+    public Set<Perfil> getPerfils() {
+        return perfils.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        perfils.add(perfil.getCod());
     }
 }
