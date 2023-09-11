fac9f1d4e600c6001c76eb01d60cb09e1d50c06b
leonardo.lopes <leonardo.lopes@spcbrasil.org.br>
2023-09-10 21:06:41 -0300

    teste git stash remoto

--- src/main/java/com/home/crudPessoa/CrudPessoaApplicationTesteLog.java
+++ src/main/java/com/home/crudPessoa/CrudPessoaApplicationTesteLog.java
@@ -16,6 +16,7 @@ public class CrudPessoaApplicationTesteLog {
 	public static void main(String[] args) {
 		String nome="AAAAAA";
 		String nomeb="AAAAAABBBBBBBBBB";
+		String nomeber="AAAAAABBBBBBBBBB";
 		log.info("Iniciando a api {} de cadastro de pessoas {}",nome,nomeb);
 		SpringApplication.run(CrudPessoaApplicationTesteLog.class, args);
 		log.info("API de cadastro de pessoas iniciada e pronto para receber requisições {} -- {}", nome,nomeb);
