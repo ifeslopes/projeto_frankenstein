dc2443f30a1184395bb204e683d5aaed79cc57b7
leonardo.lopes <leonardo.lopes@spcbrasil.org.br>
2023-09-10 21:03:09 -0300

    estudos de log.info

---
+++ src/main/java/com/home/crudPessoa/CrudPessoaApplicationTesteLog.java
@@ -0,0 +1,25 @@
+package com.home.crudPessoa;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Slf4j
+@SpringBootApplication()
+public class CrudPessoaApplicationTesteLog {
+
+	public static void main(String[] args) {
+		String nome="AAAAAA";
+		String nomeb="AAAAAABBBBBBBBBB";
+		log.info("Iniciando a api {} de cadastro de pessoas {}",nome,nomeb);
+		SpringApplication.run(CrudPessoaApplicationTesteLog.class, args);
+		log.info("API de cadastro de pessoas iniciada e pronto para receber requisições {} -- {}", nome,nomeb);
+
+	}
+
+}
